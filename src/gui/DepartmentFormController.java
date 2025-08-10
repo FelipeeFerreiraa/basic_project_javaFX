f5f948934dffb93f82e9cbb0aa453c0445149a8c
FelipeeFerreiraa <felipe333ferreira333@gmail.com>
2025-08-10 17:41:22 -0300

    Ajustes realizados, DepartmentFormController --> 10/08/25 17:41

---
+++ src/gui/DepartmentFormController.java
@@ -0,0 +1,50 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import gui.util.Constraints;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class DepartmentFormController implements Initializable {
+
+	@FXML
+	private TextField txtId;
+
+	@FXML
+	private TextField txtName;
+
+	@FXML
+	private Button btnSave;
+
+	@FXML
+	private Button btnCancel;
+
+	@FXML
+	private Label lblError;
+
+	@FXML
+	public void onBtnSaveAction() {
+		System.out.println("onBtnSaveAction");
+	}
+
+	@FXML
+	public void onBtnCancelAction() {
+		System.out.println("onBtnCancelAction");
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+	}
+
+	private void initializeNodes() {
+		Constraints.setTextFieldMaxLength(txtName, 30);
+		Constraints.setTextFieldInteger(txtId);
+	}
+
+}

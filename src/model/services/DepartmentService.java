8d9deb0367d3b3310ca841637d95988e792494a3
FelipeeFerreiraa <felipe333ferreira333@gmail.com>
2025-08-10 17:26:32 -0300

    Ajustes realizados, DepartmentForm criada e instaciado como modo Dialog --> 10/08/25 17:25

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -1,20 +1,17 @@
 package model.services;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class DepartmentService {
 
+	private DepartmentDao dao = DaoFactory.createDepartmentDao();
+
 	public List<Department> findAll() {
-		List<Department> list = new ArrayList<>();
-		list.add(new Department(1, "Tecnologia da Informação"));
-		list.add(new Department(2, "CyberSegurança"));
-		list.add(new Department(3, "Inovação e Governança"));
-		list.add(new Department(4, "Infraestrutura"));
-		list.add(new Department(5, "SAC"));
-		return list;
+		return dao.findAll();
 	}
 
 }
